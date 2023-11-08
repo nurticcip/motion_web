9f5aa2f1cb71393a31967bc1c1249b6ef28ea40a
Nurtilek <nurticcip7@gmail.com>
2023-11-08 11:54:13 +0600

    motion

---
+++ script.js
@@ -0,0 +1,15 @@
+// const img = document.querySelector(".car");
+// const p = document.querySelector(".car-text");
+
+// p.style.display = "none";
+
+// img.addEventListener("mouseover", () => {
+//   p.style.display = "block";
+//     p.style.transtion = ".5s";
+//     img.style.opacity = ".5"
+// });
+// img.addEventListener("mouseleave", () => {
+//   p.style.display = "none";
+//     p.style.transtion = ".5s";
+//     img.style.opacity = "";
+// });
